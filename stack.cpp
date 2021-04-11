e3a0e50912f10df4ee738c605f4221323fe5ec23
Pavel Prodanov <58004938+p2064@users.noreply.github.com>
2021-04-12 00:53:02 +0300

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,36 @@
+#include "stack_hpp"
+
+template <typename T>
+bool Stack<T>::isEmpty(){
+    return this->topPtr == nullptr;
+
+}
+
+template <typename T>
+void Stack<T>::push(T data){
+    Node *newNode = new Node(data);
+    newNode->prevElement = topPtr;
+    this->topPtr = newNode;
+    size++;
+
+}
+
+template <typename T>
+T Stack<T>::top(){
+    if (isEmpty()) throw -1;
+    else return topPtr->value;
+}
+
+template <typename T>
+T Stack<T>::pop(){
+    if (isEmpty()) throw -1;
+
+    else {
+        Node *tmpNode = topPtr->prevElement;
+        T tmpValue = topPtr->value;
+        delete topPtr;
+        size--;
+        topPtr = tmpNode;
+        return tmpValue;
+    }
+}
